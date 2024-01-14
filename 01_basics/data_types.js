7645603a7abf9f6cc695ba5ac88c890437b15131
Bipson Mukhiya <108604764+Bipson-Mukhiya@users.noreply.github.com>
2024-01-14 21:30:36 +0545

    Updated commit data types

---
+++ 01_basics/data_types.js
@@ -0,0 +1,31 @@
+"use script"; // Treat all JS code as newer version.
+
+// We are suing node js not browser. //
+
+// code readability must be high
+
+// javascript documentation sites 
+// tc.39.es  / mdn ** mozilla 
+
+/*
+number
+bigint
+string = ""
+boolean
+null 
+undefined
+symbol => unique
+*/
+let Name = "Bipson"
+let age = 20
+let address = 'damak'
+let Fashion = "Unique"
+
+Number = 124
+
+console.log(Number)
+console.log(Name)
+console.log(typeof boolean)
+console.log(typeof unique)
+console.log(typeof address)
+console.log(typeof null)
