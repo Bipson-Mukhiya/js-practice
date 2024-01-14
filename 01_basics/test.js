03ffa1389c03a7e391aa251e7eaef79ff405d805
Bipson Mukhiya <108604764+Bipson-Mukhiya@users.noreply.github.com>
2024-01-14 16:19:51 +0545

    Modified

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,16 @@
 console.log('Bipson Mukhiya')
+console.log("Welcome to the world of Information Technology")
+
+const accountID = 123414
+let account_password = 'Damak123'
+let account_email = "bipsonmuk1342@gmail.com"
+accountcity = "Damak"
+
+/*
+Never use a var with a keyword.
+Never apply to a bad code.
+
+*/
+
+console.log(accountcity)
+console.table([accountID,account_password,account_email,accountcity])
