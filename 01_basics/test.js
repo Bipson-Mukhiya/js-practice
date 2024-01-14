55a214253fabf231882f2f5f722c14794918e4d6
Bipson Mukhiya <108604764+Bipson-Mukhiya@users.noreply.github.com>
2024-01-14 18:23:44 +0545

    updated

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -10,7 +10,29 @@ accountcity = "Damak"
 Never use a var with a keyword.
 Never apply to a bad code.
 
+Block Scope:
+A variable declared inside a specific block (e.g., if, switch, for, while loops) is accessible only within that block
+
+Block scope is created within specific code blocks, such as conditional statements and loops
+
+Variables declared in block scope cannot escape to the outer scopes
+
+To access variables within a specific block, an object must be created for it
+
+Function Scope:
+A variable declared inside a function is accessible anywhere within that function
+
+Function scope is created when a function is defined
+
+Variables declared within a function can be accessed outside the function as well
+
+To access variables within a function, the function must be called
+
+
 */
 
-console.log(accountcity)
+
+// console.log(accountcity)
+
 console.table([accountID,account_password,account_email,accountcity])
+
