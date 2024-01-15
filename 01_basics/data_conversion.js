2868846a0f526d822a8ad07037999ee0f9a58d0c
Bipson Mukhiya <108604764+Bipson-Mukhiya@users.noreply.github.com>
2024-01-15 08:10:30 +0545

    Data content

---
+++ 01_basics/data_conversion.js
@@ -0,0 +1,83 @@
+/*
+Hello Welcome to the conversion part of the IT world.
+*/
+
+// let score = "33abc"
+// console.log(typeof score);
+// console.log(typeof (score));
+// let valueinNumber = Number(score)
+// console.log(typeof valueinNumber)
+// console.log(valueinNumber)
+
+
+// NaN refers to not a number
+
+
+// let iscore = "232ab"
+// let valueinBoolean = Boolean(iscore)
+// console.log(typeof valueinBoolean)
+// console.log(valueinBoolean)
+
+
+// let valueinString = "Good Morning"
+// let valueinNumber = Number(valueinString)
+// console.log(typeof valueinNumber)
+// console.log(valueinNumber)
+
+
+// let someNumber = 124
+// let valueofsomeNumber = String(someNumber)
+// console.log(valueofsomeNumber)
+// console.log(typeof someNumber)
+
+// let someNumber = 33
+
+// let stringNUmber = String(someNumber)
+// console.log(stringNUmber)
+// console.log(typeof stringNUmber)
+
+
+// ******************************* Operations *********************************************
+
+let value = 3
+let negValue = -value
+console.log(negValue)
+console.log(value)
+console.log(typeof negValue)
+console.log(typeof value)
+
+
+// console.log(2*3)
+// console.log(3%5)
+// console.log(6/7)
+
+// The importance of algorithm is basically important in the field of cryptography and something like that.
+
+// let str1 = "Damak"
+// let str2 = " Dharan"
+
+// let str3 = str1 + str2
+// console.log(str3)
+
+console.log("1" +2)
+console.log(1+"2")
+console.log("33"+2)
+console.log('2'+3 +4)
+console.log(3+4+"2") // Where number is added
+console.log(5 + 6 * 7 / 8) // this is not the best way to write down the code
+
+let gameCounter = 100
+++ gameCounter 
+console.log(gameCounter)
+
+
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
